97343848904ef0e17d58553d90b88e62c3d3dde4
Josh Teng <joshteng@me.com>
2016-09-07 03:33:03 +0800

    genserver synchronous. pg 138

--- processes/using_otp/metex/lib/worker.ex
+++ processes/using_otp/metex/lib/worker.ex
@@ -1,3 +1,69 @@
 defmodule Metex.Worker do
   use GenServer
+
+  ## Client API
+
+  def start_link(opts \\ []) do
+    GenServer.start_link(__MODULE__, :ok, opts)
+  end
+
+  def get_temperature(pid, location) do
+    GenServer.call(pid, {:location, location})
+  end
+
+  ## Server Callbacks
+
+  def init(:ok) do
+    {:ok, %{}}
+  end
+
+  def handle_call({:location, location},  _from, stats) do
+    case temperature_of(location) do
+      {:ok, temp} ->
+        new_stats = update_stats(stats, location)
+        {:reply, "#{temp}C", new_stats}
+      _ ->
+        {:reply, :error, stats}
+    end
+  end
+  ## Helper Functions
+  def temperature_of(location) do
+    url_for(location) |> HTTPoison.get |> parse_response
+  end
+
+  defp url_for(location) do
+    location = URI.encode(location)
+    "http://api.openweathermap.org/data/2.5/weather?q=#{location}&appid=#{apikey}"
+  end
+
+  defp parse_response({:ok, %HTTPoison.Response{status_code: 200, body: body}}) do
+    body |> JSON.decode! |> compute_temperature
+  end
+
+  defp parse_response(_) do
+    :error
+  end
+
+  defp compute_temperature(json) do
+    try do
+      temp = (json["main"]["temp"] - 273.15) |> Float.round(1)
+      {:ok, temp}
+    rescue
+      _ -> :error
+    end
+  end
+
+  defp apikey do
+    "92c12b202f29b32f7360e3838793eb05"
+  end
+
+  defp update_stats(old_stats, location) do
+    case Map.has_key?(old_stats, location) do
+      true ->
+        Map.update!(old_stats, location, &(&1 + 1))
+      false ->
+        Map.put_new(old_stats, location, 1)
+    end
+  end
+
 end
