aedcc91b3d284eb45fb19dd3b8d7e8b4a7f53795
Josh Teng <joshteng@me.com>
2016-09-07 15:56:48 +0800

    learnt about genserver

--- processes/using_otp/metex/lib/worker.ex
+++ processes/using_otp/metex/lib/worker.ex
@@ -1,14 +1,28 @@
 defmodule Metex.Worker do
   use GenServer
 
+  @name MW
+
   ## Client API
 
   def start_link(opts \\ []) do
-    GenServer.start_link(__MODULE__, :ok, opts)
+    GenServer.start_link(__MODULE__, :ok, opts ++ [name: MW])
   end
 
-  def get_temperature(pid, location) do
-    GenServer.call(pid, {:location, location})
+  def get_temperature(location) do
+    GenServer.call(@name, {:location, location})
+  end
+
+  def get_stats do
+    GenServer.call(@name, :get_stats)
+  end
+
+  def reset_stats do
+    GenServer.cast(@name, :reset_stats)
+  end
+
+  def stop do
+    GenServer.cast(@name, :stop)
   end
 
   ## Server Callbacks
@@ -26,6 +40,31 @@ defmodule Metex.Worker do
         {:reply, :error, stats}
     end
   end
+
+  def handle_call(:get_stats, _from, stats) do
+    {:reply, stats, stats}
+  end
+
+  def handle_cast(:reset_stats, _stats) do
+    {:noreply, %{}}
+  end
+
+  def handle_cast(:stop, stats) do
+    {:stop, :normal, stats} # invokes terminate
+  end
+
+  # out of band
+  def handle_info(msg, stats) do
+    IO.puts "received #{inspect msg}"
+    {:noreply, stats}
+  end
+
+  def terminate(reason, stats) do
+    IO.puts "server terminated because of #{inspect reason}"
+    inspect stats
+    :ok
+  end
+
   ## Helper Functions
   def temperature_of(location) do
     url_for(location) |> HTTPoison.get |> parse_response
