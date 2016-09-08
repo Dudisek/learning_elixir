e7ada0c5eb8af8ce0d2c8b03cd629a05890476da
Josh Teng <joshteng@me.com>
2016-09-09 03:46:45 +0800

    WIP link monitoring

--- processes/using_otp/ring/lib/ring.ex
+++ processes/using_otp/ring/lib/ring.ex
@@ -5,12 +5,37 @@ defmodule Ring do
 
   def loop do
     receive do
-      {link, link_to} when is_pid(link_to) ->
+      {:link, link_to} when is_pid(link_to) ->
         Process.link(link_to)
+        loop
+
+      :trap_exit ->
+        IO.puts "Trapping exits"
+        Process.flag(:trap_exit, true)
         loop
 
       :crash ->
         1/0
+
+      {:EXIT, pid, reason} ->
+        IO.puts "#{inspect self} received {:EXIT, #{inspect pid}, #{reason}}"
+        loop
     end
+  end
+
+  def link_processes(procs) do
+    link_processes(procs, [])
+  end
+
+  def link_processes([proc_1, proc_2|rest], linked_processes) do
+    send(proc_1, {:link, proc_2})
+    link_processes([proc_2|rest], [proc_1|linked_processes])
+  end
+
+  def link_processes([proc|[]], linked_processes) do
+    IO.puts "link 2"
+    first_process = linked_processes |> List.last
+    send(proc, {:link, first_process})
+    :ok
   end
 end
